c53032196123bd73c8a926038c13bfd338d76cba
Tim Roberts <tim@mck-p.com>
2024-04-27 15:59:15 -0400

    Add meta description to single thought

--- src/view/layouts/single-thought.ts
+++ src/view/layouts/single-thought.ts
@@ -9,6 +9,7 @@ const SingleThoughtLayout = ({
 }: {
   meta: {
     title: string
+    description?: string
   }
   styles: string[]
   headLinks: LinkRequest[]
@@ -22,6 +23,11 @@ const SingleThoughtLayout = ({
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>${meta.title}</title>
+    ${
+      meta.description
+        ? `<meta name="description" content="${meta.description}" >`
+        : ''
+    }
     <link rel="apple-touch-icon" sizes="180x180" href="/assets/images/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/assets/images/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/assets/images/favicon-16x16.png">
