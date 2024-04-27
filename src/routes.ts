9cf2d97cd5f70635c4301d4f054e97687fcd8877
Tim Roberts <tim@mck-p.com>
2024-04-27 15:48:10 -0400

    Move routes to own file

---
+++ src/routes.ts
@@ -0,0 +1,162 @@
+import * as Thoughts from '@app/domains/thoughts'
+import * as Pages from '@app/view/pages'
+import * as Route from './server/route'
+import SingleThoughtLayout from '@app/view/layouts/single-thought'
+
+export const adminRoutes = [
+  Route.GET({
+    path: '/admin/thoughts/create',
+    handlers: (ctx) => {
+      ctx
+        .registerHeadScript({
+          src: 'https://cdn.tiny.cloud/1/9gwptj7qlzokszivkumi1duezsyjy0wp3c3u5zb772libeo7/tinymce/7/tinymce.min.js',
+          referrerpolicy: 'origin',
+        })
+        .registerHeadScript({
+          src: 'https://cdn.jsdelivr.net/npm/bulma-extensions@6.2.7/bulma-quickview/dist/js/bulma-quickview.min.js',
+        })
+        .registerStyle(
+          'https://cdn.jsdelivr.net/npm/bulma-extensions@6.2.7/dist/css/bulma-extensions.min.css',
+        )
+        .registerFootScript({
+          src: '/assets/js/editor.js',
+        })
+        .render(Pages.CreateThought(), {
+          title: 'Create | Tim on a Path',
+        })
+    },
+  }),
+  Route.GET({
+    path: '/admin/thoughts/:id/edit',
+    handlers: async (ctx) => {
+      const thought = await Thoughts.getById(ctx.params.id)
+
+      if (!thought) {
+        ctx.status = 404
+        ctx.body = 'Not Found'
+        return
+      }
+
+      ctx
+        .registerHeadScript({
+          src: 'https://cdn.tiny.cloud/1/9gwptj7qlzokszivkumi1duezsyjy0wp3c3u5zb772libeo7/tinymce/7/tinymce.min.js',
+          referrerpolicy: 'origin',
+        })
+        .registerHeadScript({
+          src: 'https://cdn.jsdelivr.net/npm/bulma-extensions@6.2.7/bulma-quickview/dist/js/bulma-quickview.min.js',
+        })
+        .registerStyle(
+          'https://cdn.jsdelivr.net/npm/bulma-extensions@6.2.7/dist/css/bulma-extensions.min.css',
+        )
+        .registerFootScript({
+          src: '/assets/js/editor.js',
+        })
+        .render(Pages.EditThought({ thought }), {
+          title: `Edit ${thought.title} | Tim on a Path`,
+        })
+    },
+  }),
+]
+
+export const publicRoutes = [
+  Route.GET({
+    path: '/.well-known/healthcheck',
+    handlers: (ctx) => {
+      ctx.status = 200
+      ctx.body = {
+        data: {
+          healthy: true,
+        },
+      }
+    },
+  }),
+  Route.GET({
+    path: '/',
+    handlers: async (ctx) => {
+      ctx.registerStyle('/assets/css/home.css')
+      const recentThoughts = await Thoughts.getRecentThoughts({ limit: 5 })
+
+      const meta = {
+        title: 'Tim on a Path',
+        description:
+          'We are all on a path somewhere. This is where Tim writes their thoughts about the path they are on.',
+      }
+
+      ctx.render(Pages.HomePage({ recentThoughts }), meta)
+    },
+  }),
+  Route.GET({
+    path: '/thoughts',
+    handlers: async (ctx) => {
+      ctx.registerStyle('/assets/css/home.css')
+      const thoughts = await Thoughts.getRecentThoughts({ limit: 10 })
+
+      const meta = {
+        title: 'Thoughts | Tim on a Path',
+        description:
+          'These are the most recent Thoughts that I have written about.',
+      }
+
+      ctx.render(Pages.Thoughts({ thoughts }), meta)
+    },
+  }),
+  Route.GET({
+    path: '/thoughts/:id',
+    handlers: async (ctx) => {
+      ctx.registerStyle('/assets/css/home.css').setLayout(SingleThoughtLayout)
+
+      const thought = await Thoughts.getById(ctx.params.id)
+
+      const meta = {
+        title: `${thought.title} | Tim on a Path`,
+        description: thought.description ?? 'Another thought about the path',
+      }
+
+      ctx.render(Pages.Thought({ thought }), meta)
+    },
+  }),
+]
+
+export const apiRoutes = [
+  Route.POST({
+    path: '/api/thoughts',
+    handlers: async (ctx) => {
+      const result = await Thoughts.createThought(
+        Thoughts.CreateThoughtSchema.parse(ctx.request.body),
+      )
+
+      ctx.status = 201
+
+      ctx.body = {
+        data: result,
+      }
+    },
+  }),
+  Route.GET({
+    path: '/api/thoughts/:id',
+    handlers: async (ctx) => {
+      const result = await Thoughts.getById(ctx.params.id)
+
+      ctx.status = 200
+
+      ctx.body = {
+        data: result,
+      }
+    },
+  }),
+  Route.PATCH({
+    path: '/api/thoughts/:id',
+    handlers: async (ctx) => {
+      const result = await Thoughts.updateThoughtById(
+        ctx.params.id,
+        ctx.request.body,
+      )
+
+      ctx.status = 200
+
+      ctx.body = {
+        data: result,
+      }
+    },
+  }),
+]
